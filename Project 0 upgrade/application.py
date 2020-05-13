a8860bca38b559c1106ff64b2320cc52a976c9bc
cdaddy74 <curtisacraig@gmail.com>
2020-05-13 01:21:39 -0400

    Pages are linked

--- Project 0 upgrade/application.py
+++ Project 0 upgrade/application.py
@@ -3,5 +3,18 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index.html")
 def index():
 	return render_template("index.html")
+
+@app.route("/forms.html")
+def forms():
+	return render_template("forms.html")
+
+@app.route("/lookup_customers.html")
+def lookup_customers():
+	return render_template("lookup_customers.html")
+
+@app.route("/check_class_attendees.html")
+def check_class_attendees():
+	return render_template("check_class_attendees.html")
